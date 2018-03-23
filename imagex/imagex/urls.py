f250ee9ef0a90ff5c325b15c079d9db34805071a
qiullina <36856102+qiullina@users.noreply.github.com>
2018-03-23 20:15:02 +0800

    Add files via upload

---
+++ imagex/imagex/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from upload import views
+
+urlpatterns = [
+    url('^$', views.home, name='home'),
+    path('upload/', include('upload.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
