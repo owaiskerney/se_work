d4506c13f3a8b769bb6e70fcc8f058476b3526b0
Owais <owaiskerney@gmail.com>
2018-03-26 14:27:00 +0800

    Owais1

--- imagex/imagex/urls.py
+++ imagex/imagex/urls.py
@@ -8,6 +8,7 @@ from upload import views
 urlpatterns = [
     url('^$', views.home, name='home'),
     path('upload/', include('upload.urls')),
+    path('search/', include('search.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
