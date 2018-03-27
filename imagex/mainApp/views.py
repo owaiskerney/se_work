04e0bac720075ea7ab71bf1961ac593de3d3012e
qiullina <36856102+qiullina@users.noreply.github.com>
2018-03-27 21:23:51 +0800

    Update views.py
    upload & login update

--- imagex/mainApp/views.py
+++ imagex/mainApp/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-
 from datetime import datetime, timedelta, date
-
-
 from django.core.validators import validate_email
 from django.db.models import Max
 from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
 from django.contrib.auth.hashers import make_password, check_password
+from upload.validators import validate_file_extension
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.contrib.auth import login, authenticate
+from django.template import loader
+from django.contrib.auth.models import User
+import json
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from upload.models import *
+from upload.forms import ImageForm
+
 
 
 
@@ -20,11 +29,59 @@ from django.contrib.auth.hashers import make_password, check_password
 def index(request):
 	return HttpResponse("This is main app")
 
+#login 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            return redirect(upload)
+        else:
+            return render(request,'login.html', {'feedback':json.dumps("Please input correct username and password!")})   
+    return render(request,'login.html')
+
 
 # Upload view
+@login_required
 def upload(request):
-	#if not isAuthenticated(request):
-	return redirect('/home')
+    if request.method == 'POST':
+        form= ImageForm(request.POST, request.FILES)
+        title = request.POST.get('title')
+        tag = request.POST.get('tag')
+        description = request.POST.get('description')
+        tag_list = tag.split(',')
+        for tag in tag_list: 
+            if not Tag.objects.filter(name=tag):
+                new_tag = Tag(name=tag)
+                new_tag.save()
+
+        total = Image.objects.filter(owner=request.user).count()
+        frequency = Image.objects.filter(owner=request.user, uploadtime=datetime.date.today()).count()
+       
+        if form.is_valid() and total < 100 and frequency < 100:
+            cd = form.cleaned_data
+            new_item=form.save(commit=False)
+            new_item.owner = request.user
+            new_item.title = title 
+            new_item.description = description          
+            new_item.save()  
+            for item in tag_list:
+                new_item.tag.add(Tag.objects.get(name=item))         
+            return redirect(home)
+        elif not form.is_valid():
+            form=ImageForm()
+            return render(request,'upload.html', {'form':form, 'feedback':json.dumps("Please submit JPEG file!")})   
+        elif total >= 100:
+            form=ImageForm()
+            return render(request,'upload.html', {'form':form, 'feedback':json.dumps("You are only allowed to maintain 3 images!")})   
+        elif frequency >= 100: 
+            form=ImageForm()
+            return render(request,'upload.html', {'form':form, 'feedback':json.dumps("You are only allowed to upload 4 images per day!")})    
+    else:
+        form = ImageForm()
+    return render(request, 'upload.html', {'form': form})
+
 
 
 # Search view
