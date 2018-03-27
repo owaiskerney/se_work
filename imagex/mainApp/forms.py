4087bf1acb4df21d437c880d0f331e9789bf9db2
qiullina <36856102+qiullina@users.noreply.github.com>
2018-03-27 21:25:14 +0800

    Create forms.py
    Imageform

---
+++ imagex/mainApp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from upload.models import Image, Category
+from django.shortcuts import get_object_or_404
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['title','category', 'description', 'image', ]
+
+    def save(self, force_insert=False,force_update=False, commit=True):
+    	image=super(ImageForm,self).save(commit=False)
+
+    	if commit:
+    		image.save()
+    	return image
