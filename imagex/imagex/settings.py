10e8cd61ea66adf3496b45b5d1e0067237a61e3e
owaiskerney <owaiskerney@gmail.com>
2018-03-28 02:46:30 +0800

    Changes

--- imagex/imagex/settings.py
+++ imagex/imagex/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-SITE_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+STATIC_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (  
     os.path.join(BASE_DIR, 'static/'),  
