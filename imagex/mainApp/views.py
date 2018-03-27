d8b041f6e3d7b83c78cfc0f6d66a1d01f1b6c87c
Owais <owaiskerney@gmail.com>
2018-03-27 20:39:50 +0800

    Adding

--- imagex/mainApp/views.py
+++ imagex/mainApp/views.py
@@ -13,7 +13,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from django.contrib.auth.hashers import make_password, check_password
-from .functions import *
+
 
 
 
@@ -24,13 +24,44 @@ def index(request):
 # Upload view
 def upload(request):
 	#if not isAuthenticated(request):
-	return redirect('/index')
+	return redirect('/home')
 
 
 # Search view
 def search(request):
 	#if not isAuthenticated(request):
-	return redirect('/index')
+	#find tag in Tag
+	#use tag_id to get corresponding image_id in many to many table
+	#keep using image_ids obtained from this query to return all images to front end
+	tag_id_found= tag_id in Tag.objects.filter(name= tag)
+	
+
+	#if tag not found
+	#if (tag_id_found== ):
+		#Implement tag not found
+
+	#if tag found, look for corresponding image_id
+	image_id_list=[it.image_id for it in Image.Tag.filter(tag_id=tag_id_found)]
+
+	#Connect images to front end to load them
+	image_results=[]
+	for i in image_id_list:
+		image_results.append()
+
+
+
+	#return the search.html page with laoded images 
+
+
+
+	context={}
+
+
+
+
+	return render(request, 'mainApp/Search.html', context)
+
+	#return redirect('/home')
 
 
 #tags = Tag.objects.all()
