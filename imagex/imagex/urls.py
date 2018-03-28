2a6097a8775d4d456103d5abfce5900eec7a569c
qiullina <36856102+qiullina@users.noreply.github.com>
2018-03-28 17:40:01 +0800

    Update urls.py

--- imagex/imagex/urls.py
+++ imagex/imagex/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^login/',views.login, name='login'),
     url(r'^upload/', views.upload, name='upload'),
     url(r'^search/', views.search, name='search'),
-    url(r'^myaccount/',views.account, name='account')
+    url(r'^myaccount/',views.myaccount, name='myaccount')
 
     #url('', include('mainApp.urls'))
 ]
