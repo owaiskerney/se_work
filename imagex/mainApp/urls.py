c2357072ec66f874ccf2a6faef219e521488dacd
Inaqiu <wysqll@gmail.com>
2018-04-17 17:06:54 +0800

    delete imagex dir

--- imagex/mainApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-
-urlpatterns = [
- #url(r'^upload$', views.upload, name='upload'),
- #url(r'^search$', views.search, name='search' )
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
